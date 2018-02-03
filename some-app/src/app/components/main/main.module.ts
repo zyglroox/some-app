ea6721c5b1ab7c36a80a949ee280cc2870c73669
zyglroox <horseofbarberry67@gmail.com>
2018-02-04 00:45:27 +0300

    flexes try

--- some-app/src/app/components/main/main.module.ts
+++ some-app/src/app/components/main/main.module.ts
@@ -1,10 +1,11 @@
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MainComponent } from './main.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import mainRuotes from './main.ruotes';
 
 @NgModule({
-    imports: [CommonModule, mainRuotes],
+    imports: [CommonModule, mainRuotes, FlexLayoutModule],
     declarations: [MainComponent]
 })
 export default class MainModule {}
